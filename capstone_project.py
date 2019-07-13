229f589d2ef825eb7382efc3c8d22d12bb360d77
fguo0422 <52810976+fguo0422@users.noreply.github.com>
2019-07-12 22:45:09 -0400

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# # This notebook will be used for IBM Data Science capstone project
+
+# In[2]:
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
