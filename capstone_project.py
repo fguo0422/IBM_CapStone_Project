1eb59be480468c1dde59762839c0e76d5076f217
Tiegang Fang PhD <tfang2@wolftech.ad.ncsu.edu>
2019-08-17 20:41:45 -0400

    git@github.com:fguo0422/IBM_CapStone_Project.git

--- capstone_project.py
+++ capstone_project.py
@@ -1,16 +1,17 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-<h1> This notebook will be used for IBM Data Science capstone project</h1>
+# # This is the notebook of Course 9 capstone project  for  IBM Data Science Certificate Program
 
-# In[2]:
+# In[4]:
 
+
+# Import the pandas library as pd.
 import pandas as pd
+# Import the numpy library as np.
 import numpy as np
+# Print the following the statement: Hello Capstone Project Course
 print ("Hello Capstone Project Course")
 
 
 # In[ ]:
-
-
-
